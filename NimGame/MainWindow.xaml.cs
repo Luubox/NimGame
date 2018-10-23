382686a1fb35cb198f6ababe55c0b11cff3f8d82
J Nielsen <buhros@hotmail.com>
2018-10-23 12:51:41 +0200

    Ebbe

--- NimGame/MainWindow.xaml.cs
+++ NimGame/MainWindow.xaml.cs
@@ -19,11 +19,35 @@ namespace NimGame
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+
     {
+
+        List<TextBlock> row1Blocks = new List<TextBlock>();
+        List<TextBlock> row2Blocks = new List<TextBlock>();
+        List<TextBlock> row3Blocks = new List<TextBlock>();
+        List<TextBlock> row4Blocks = new List<TextBlock>();
+
         public MainWindow()
         {
             InitializeComponent();
+            TextBlock t = new TextBlock();
+            row1Blocks.Add(t);
+            t.Text = "bob";
+            WindowGrid.Children.Add(t);
+            t.SetValue(Grid.RowProperty, 0);
+            t.SetValue(Grid.ColumnProperty, 3);
+
         }
 
+        int[] myArray = new int[4] {1, 3, 5, 7};
+
+        private void Row1Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (row1Blocks.Count > 0)
+            {
+                WindowGrid.Children.Remove(row1Blocks[0]);
+                row1Blocks.RemoveAt(0);
+            }
+        }
     }
 }
